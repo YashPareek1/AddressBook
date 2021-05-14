b76bfd7d001c8e9ee9d18102c7885c5e67f13358
YashPareek1 <yashpareekg4@gmail.com>
2021-05-15 00:11:01 +0530

    address book

---
+++ com/AddressBook.java
@@ -0,0 +1,10 @@
+package com;
+
+public class AddressBook
+{
+	public static void main(String[] args)
+	{
+	System.out.println("Welcome to Addressbook");
+System.out.println("Welcome to User Registration Page");
+	}
+}
---
+++ com/Contact.java
@@ -0,0 +1,5 @@
+package com;
+
+public class Contact {
+System.out.println("WElcome to contact");
+}
